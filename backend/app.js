8abd863c58f306b5666d784a84ef9adffa23eb98
ayo <12ayo22@gmail.com>
2021-07-31 22:28:59 +0900

    [faet] multer로 이미지를 업로드받는 api 만들기

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,37 @@ app.get('/', (req, res) => {
   res.json({ msg: 'OK' });
 });
 
+// multer 설정
+const upload = multer({
+  storage: multer.diskStorage({
+    // 저장할 장소
+    destination(req, file, cb) {
+      cb(null, 'public/uploads');
+    },
+    // 저장할 이미지의 파일명
+    filename(req, file, cb) {
+      const ext = path.extname(file.originalname); // 파일의 확장자
+      console.log('file.originalname', file.originalname);
+      // 파일명이 절대 겹치지 않도록 해줘야한다.
+      // 파일이름 + 현재시간밀리초 + 파일확장자명
+      cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    }
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 } // 파일 크기 제한
+});
+
+// 하나의 이미지 파일만 가져온다.
+app.post('/img', upload.single('img'), (req, res) => {
+  // 해당 라우터가 정상적으로 작동하면 public/uploads에 이미지가 업로드된다.
+  // 업로드된 이미지의 URL 경로를 프론트엔드로 반환한다.
+  console.log('전달받은 파일', req.file);
+  console.log('저장된 파일의 이름', req.file.filename);
+  res.json({
+    URL: '테스트중^^'
+  });
+});
+
+// 포트는 임의로 4050으로 사용
 app.listen(4050, () => {
   console.log('4050번 포트에서 대기 중~');
 });
